81c130a12a7b54c6eb77a493204d80724f807c98
Alexander Pyreev <PyreevAS@msk.bcs.ru>
2019-02-26 10:32:12 +0300

    lesson 53 - error processing fetch

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 
 const getResource = async (url) => {
   const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, received ${res.status}`);
+  }
   const body = await res.json();
   return body;
 };
 
-getResource('https://swapi.co/api/people/1/')
+getResource('https://swapi.co/api/people/45345/')
   .then((body) => {
     console.log(body);
+  })
+  .catch((err) => {
+    console.log('Could not fetch', err);
   });
 
 ReactDOM.render(<div />, document.getElementById('root'));
