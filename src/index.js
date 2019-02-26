41a95c49ae2134f3d17af43d0256e7369e4aabe9
Alexander Pyreev <PyreevAS@msk.bcs.ru>
2019-02-26 10:25:25 +0300

    lesson 51 - fetch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const getResource = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+};
+
+getResource('https://swapi.co/api/people/1/')
+  .then((body) => {
+    console.log(body);
+  });
+
 ReactDOM.render(<div />, document.getElementById('root'));
