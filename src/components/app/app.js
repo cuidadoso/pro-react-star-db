4621e00eedc29fcd1511331f39bf66cb5211ba32
Alexander Pyreev <PyreevAS@msk.bcs.ru>
2019-02-26 15:11:16 +0300

    lesson 69 - componentDidCatch

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,33 +2,41 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
+import ErrorButton from '../error-button';
+import ErrorIndicator from '../error-indicator';
+
+import './app.css';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 
-import './app.css';
-
 export default class App extends Component {
+
   state = {
     showRandomPlanet: true,
-    selectedPerson: null
+    hasError: false
   };
 
   toggleRandomPlanet = () => {
     this.setState((state) => {
       return {
         showRandomPlanet: !state.showRandomPlanet
-      };
+      }
     });
   };
 
-  onPersonSelected = (id) => {
-    this.setState({
-      selectedPerson: id
-    });
-  };
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
 
   render() {
-    const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
+
+    if (this.state.hasError) {
+      return <ErrorIndicator />
+    }
+
+    const planet = this.state.showRandomPlanet ?
+      <RandomPlanet/> :
+      null;
 
     return (
       <div className="stardb-app">
@@ -41,6 +49,7 @@ export default class App extends Component {
         >
           Toggle Random Planet
         </button>
+        <ErrorButton/>
 
         <div className="row mb2">
           <div className="col-md-6">
