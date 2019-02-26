1637523e9d5c94c3be42335d772397d73fe96c7d
Alexander Pyreev <PyreevAS@msk.bcs.ru>
2019-02-26 14:11:35 +0300

    lesson 68 - componentWillUnmount

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -17,10 +17,11 @@ export default class RandomPlanet extends Component {
   componentDidMount() {
     this.updatePlanet();
     this.interval = setInterval(this.updatePlanet, 15000);
-    // clearInterval(this.interval);
   }
 
-  componentWillUnmount() {}
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
   onPlanetLoaded = (planet) => {
     this.setState({
